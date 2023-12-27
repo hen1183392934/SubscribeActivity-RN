4af09df9e5347cf74c21bac5281ef5eaea4e9789
WENG- <1183392934@qq.com>
2023-12-27 15:08:36 +0800

    Add files via upload

---
+++ assets/icons/icomoon.ts
@@ -0,0 +1,10 @@
+// import { createIconSetFromIcoMoon } from 'react-native-vector-icons'
+import { createIconSetFromIcoMoon } from '@expo/vector-icons';
+const Icon = createIconSetFromIcoMoon(require('./selection.json'), 'IcoMoon', 'icomoon.ttf')
+export default Icon
+
+// export const Button = Icon.Button
+// export const TabBarItem = Icon.TabBarItem
+// export const TabBarItemIOS = Icon.TabBarItemIOS
+// export const ToolbarAndroid = Icon.ToolbarAndroid
+// export const getImageSource = Icon.getImageSource
